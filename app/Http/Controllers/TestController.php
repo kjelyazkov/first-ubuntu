6943e861a96917f7e75283cc0365e181ddb34509
kjelazkov <kjelyazkovnew@gmail.com>
2021-05-22 11:12:44 +0300

    newMe from dev

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,7 +11,11 @@ class TestController extends Controller
         for ($i=0; $i < 10; $i++) { 
             $users[] = 'I am ' . $i . 'user';
         }
+        
         $something = 'Im a new person';
+
+        $newMe = 'from dev';
+        
         return view('welcome', compact('users', 'something'));
     }
 }
